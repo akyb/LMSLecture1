658a9a7a6b056c87c14abab8bf503f77d7464a50
akyb <akbar.hosseinkhani@gmail.com>
2016-12-16 16:55:44 -0500

    first commit

---
+++ src/main/java/com/gcit/dao/AuthorDao.java
@@ -0,0 +1,57 @@
+package com.gcit.dao;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.stereotype.Component;
+
+import com.gcit.domain.Author;
+import com.mysql.jdbc.Connection;
+
+@Component
+public class AuthorDao {
+
+	// @Autowired
+	// JdbcTemplate jdbcTemplate;
+
+	public Connection getConnection() throws SQLException {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		Connection conn = (Connection) DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/library",
+				"root", "root");
+		return conn;
+	}
+
+	public Author getAuthorById(int authorId) throws SQLException {
+		
+		String sql = "SELECT * FROM library.tbl_author where authorId=?";
+		PreparedStatement prepareStatement = getConnection().prepareStatement(sql);
+		prepareStatement.setInt(1, authorId);
+		ResultSet resultSet = prepareStatement.executeQuery();
+		Author author = new Author();
+		while(resultSet.next()){
+			author.setAuthorId(resultSet.getInt("authorId"));
+			author.setAuthorName(resultSet.getString("authorName"));
+		}
+		return author;
+	}
+}
+
+// class AuthorRowMapper implements RowMapper<Author>
+// {
+// public Author mapRow(ResultSet rs, int rowNum) throws SQLException {
+// Author author = new Author();
+// author.setAuthorId(rs.getInt("authorId"));
+// author.setAuthorName("authorName");
+// return author;
+// }
+//
+// }
